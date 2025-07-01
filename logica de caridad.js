65596bb113c08bb0d4b320b3b6361a2b90d97f05
fogertupadre <masterpablo504@gmail.com>
2025-07-01 12:05:22 -0500

    Add files via upload

---
+++ logica de caridad.js
@@ -0,0 +1,10 @@
+function ingreso(){
+    let usuario ,contraseña;
+    do{
+        usuario=prompt("ingrese su usuario");
+        contraseña=prompt("ingrese su contraseña");
+    }while(usuario !== "pedro" && contraseña !== "entrar")
+}
+function mostrar(){
+    
+}
